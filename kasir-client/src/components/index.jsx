05894b0e4db185ea4e92e3f784eea421df34282b
Frodewald Grimbert <67095670+frodewald@users.noreply.github.com>
2024-12-03 16:19:40 +0700

    Delete kasir-client directory

--- kasir-client/src/components/index.jsx
+++
@@ -1,6 +0,0 @@
-import NavbarComponent from './NavbarComponent'
-import ListCategories from './ListCategories'
-import Hasil from './Hasil'
-import Menus from './Menus'
-
-export {NavbarComponent, ListCategories, Hasil, Menus}
